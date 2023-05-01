2845a537fd254edf3519f18360cb261c778f6d6d
Uma Shankar Panigrahi <umashankarpanigrahi1927@gmail.com>
2023-05-01 21:24:29 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from './mongodb/connect.js';
+import userRoutes from './routes/userRoutes.js'
+import postRoutes from './routes/postRoutes.js'
+import orderRoutes from './routes/orderRoutes.js';
+import resetPasswordRoutes from './routes/resetPasswordRoutes.js'
+import commentRoutes from './routes/commentRoutes.js'
+import signupOtpRoutes from './routes/signupOtpRoutes.js'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/user',userRoutes)
+app.use('/post',postRoutes)
+app.use('/order',orderRoutes);
+app.use('/reset-password',resetPasswordRoutes);
+app.use('/comment',commentRoutes);
+app.use('/signupotp',signupOtpRoutes);
+
+app.get('/',async(req,res) => {
+    res.sendStatus(200);
+})
+
+const startServer = async () => {
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, ()=>console.log('server has started on http://localhost:8080'));
+    }catch(error){
+        console.log(error);
+    }
+} 
+startServer();
